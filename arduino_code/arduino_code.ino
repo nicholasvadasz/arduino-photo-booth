430ddd5f1d4a3ca5be9826d5861e075ca11850ff
pellingerc <77890183+pellingerc@users.noreply.github.com>
2023-12-11 21:45:25 -0500

    Add files via upload

--- arduino_code/arduino_code.ino
+++ arduino_code/arduino_code.ino
@@ -1,4 +1,5 @@
 #include "arduino_code.h"
+#include <Adafruit_SleepyDog.h>
 
 const int toggleFilterButtonPin = 0;         // Button connected to digital pin 0
 const int takePhotoButtonPin = 1;            // Another button connected to digital pin 1
@@ -6,13 +7,17 @@ const int brightnessPotentiometerPin = A1;   // Potentiometer 1 connected to ana
 const int saturationPotentiometerPin = A2;  // Potentiometer 2 connected to analog pin A2
 const int brightnessLedPin = 6;              // LED connected to digital pin 6
 const int saturationLedPin = 7;              // LED connected to digital pin 5
-const int takingPhotoLedPin = 8;    
+const int takingPhotoLedPin = 8;   
+unsigned long lastStateChangeTime = 0;
+const int stateTimeout = 30000;   
     // LED connected to digital pin 4
 
 bool takePhoto = false;
 bool renderDone = false;
 bool firstPhoto = true;
+bool cat = false;
 filter currFilterSetting = NO_FILTER;
+static state CURRENT_STATE = sWAIT_FOR_INPUT;
 
 void setup() {
   Serial.begin(9600);
@@ -21,10 +26,11 @@ void setup() {
   pinMode(brightnessLedPin, OUTPUT);
   pinMode(saturationLedPin, OUTPUT);
   pinMode(takingPhotoLedPin, OUTPUT);
+  Watchdog.enable(8000); 
 }
 
 void loop() {
-  static state CURRENT_STATE = sWAIT_FOR_INPUT;
+  Watchdog.reset();
   if (digitalRead(takePhotoButtonPin) == HIGH && CURRENT_STATE == sWAIT_FOR_INPUT) {
     takePhoto = true;
   }
@@ -34,6 +40,12 @@ void loop() {
   updateLEDs();
   CURRENT_STATE = updateFSM(CURRENT_STATE, millis());
   delay(10);
+  if (millis() - lastStateChangeTime >= stateTimeout) {
+    // Call your ISR here
+    catISR();
+    // Reset the time to avoid triggering the ISR continuously
+    lastStateChangeTime = millis();
+  }
 }
 
 void toggleFilter() {
@@ -57,6 +69,19 @@ void toggleFilter() {
       currFilterSetting = NO_FILTER;
       break;
   }
+}
+
+void catISR(){
+  Serial.print(currFilterSetting);
+  Serial.print(" ");
+  Serial.print(analogRead(brightnessPotentiometerPin));
+  Serial.print(" ");
+  Serial.print(analogRead(saturationPotentiometerPin));
+  Serial.print(" ");
+  Serial.print(0);
+  Serial.print(" ");
+  Serial.println(1);
+  cat = true;
 }
 
 void updateLEDs() {
@@ -88,20 +113,37 @@ state updateFSM(state curState, unsigned long mils) {
         Serial.print(" ");
         Serial.print(analogRead(saturationPotentiometerPin));
         Serial.print(" ");
-        Serial.println(1);
+        Serial.print(1);
+        Serial.print(" ");
+        Serial.println(0);
         takePhoto = false;
         firstPhoto = false;
-        nextState = sCOMMUNICATING;
+        nextState = sWAIT_FOR_ACK;
       } else {
         if (!firstPhoto) {
-          Serial.print(currFilterSetting);
-          Serial.print(" ");
-          Serial.print(analogRead(brightnessPotentiometerPin));
-          Serial.print(" ");
-          Serial.print(analogRead(saturationPotentiometerPin));
-          Serial.print(" ");
-          Serial.println(0);
+          if (cat) {
+            Serial.print(currFilterSetting);
+            Serial.print(" ");
+            Serial.print(analogRead(brightnessPotentiometerPin));
+            Serial.print(" ");
+            Serial.print(analogRead(saturationPotentiometerPin));
+            Serial.print(" ");
+            Serial.print(0);
+            Serial.print(" ");
+            Serial.println(1);
+          } else {
+            Serial.print(currFilterSetting);
+            Serial.print(" ");
+            Serial.print(analogRead(brightnessPotentiometerPin));
+            Serial.print(" ");
+            Serial.print(analogRead(saturationPotentiometerPin));
+            Serial.print(" ");
+            Serial.print(0);
+            Serial.print(" ");
+            Serial.println(0);
           }
+          
+        }
         nextState = sWAIT_FOR_INPUT;
       }
       break;
