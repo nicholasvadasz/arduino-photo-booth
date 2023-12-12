fe76a646a82cdd071ac6b69e5c27c469bd07d79a
Nicholas Vadasz <nicholas_vadasz@brown.edu>
2023-12-11 21:12:59 -0500

    variable use as brightness and saturation

--- src/App.tsx
+++ src/App.tsx
@@ -7,13 +7,26 @@ function App() {
   const [variables, setVariables] = useState([0, 0]);
   const [version, setVersion] = useState("grayscale");
 
+  const mapper = (value: number) => {
+    switch (value) {
+      case 0:
+        return "grayscale";
+      case 1:
+        return "swirl";
+      case 2:
+        return "mirror";
+      default:
+        return "swirl";
+    }
+  }
+
   useEffect(() => {
     fetch("/variables.txt")
       .then((response) => response.text())
       .then((text) => {
         const variables = text.split(" ");
-        setVariables([variables[0] as unknown as number, variables[1] as unknown as number]);
-        //setVersion(variables[2].trim());
+        setVersion(mapper(variables[0].trim() as unknown as number));
+        setVariables([variables[1].trim() as unknown as number, variables[2].trim() as unknown as number]);
       });
   }, []);
 
@@ -22,8 +35,8 @@ function App() {
       <h1 style={{ fontFamily: "Comic Sans MS" }}>Arduino Photobooth!</h1>
       <div className="outerBevel">
         <div className="flatSurface">
-          <div className="innerBevel" style={{ height: "45vh", width: "36vw" }}>
-            <ShaderImage imagePath="/image.jpg" version={version} />
+          <div className="innerBevel" style={{ height: "45vh", width: "38vw" }}>
+            <ShaderImage imagePath="/image.jpg" version={version} variables={variables}/>
           </div>
         </div>
       </div>
--- src/ShaderImage.tsx
+++ src/ShaderImage.tsx
@@ -3,84 +3,108 @@ import { Canvas } from '@react-three/fiber';
 import { TextureLoader } from 'three';
 import { Plane } from '@react-three/drei';
 
-const ShaderImage = ({ imagePath, version }: { imagePath: string, version: string }) => {
+const ShaderImage = ({ imagePath, version, variables }: { imagePath: string, version: string, variables: number[] }) => {
+
+// [0.0, 2.0] values for brightness and saturation
+const brightness = variables[0] / 500; 
+const saturation = variables[1] / 500; 
+
+const baseShader = {
+  uniforms: {
+    uTexture: { value: new TextureLoader().load(imagePath) },
+    uBrightness: { value: brightness },
+    uSaturation: { value: saturation },
+  },
+  vertexShader: `
+    varying vec2 vUv;
+    void main() {
+      vUv = uv;
+      gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+    }
+  `,
+};
+
+const adjustImage = `
+  vec3 adjustBrightness(vec3 color, float brightness) {
+    return color * brightness;
+  }
+
+  vec3 adjustSaturation(vec3 color, float saturation) {
+    float gray = dot(color, vec3(0.299, 0.587, 0.114));
+    return mix(vec3(gray), color, saturation);
+  }
+`;
 
 const grayscale = {
-    uniforms: {
-      uTexture: { value: new TextureLoader().load(imagePath) },
-    },
-    vertexShader: `
-      varying vec2 vUv;
-      void main() {
-        vUv = uv;
-        gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
-      }
-    `,
-    fragmentShader: `
-      precision mediump float;
-      varying vec2 vUv;
-      uniform sampler2D uTexture;
-      void main() {
-        vec4 color = texture2D(uTexture, vUv);
-        float gray = dot(color.rgb, vec3(0.299, 0.587, 0.114));
-        gl_FragColor = vec4(vec3(gray), color.a);
-      }
-    `
-  };
+  ...baseShader,
+  fragmentShader: `
+    precision mediump float;
+    varying vec2 vUv;
+    uniform sampler2D uTexture;
+    uniform float uBrightness;
+    uniform float uSaturation;
 
-  const swirl = {
-    uniforms: {
-      uTexture: { value: new TextureLoader().load(imagePath) },
-    },
-    vertexShader: `
-      varying vec2 vUv;
-      void main() {
-        vUv = uv;
-        gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
-      }
-    `,
-    fragmentShader: `
-      precision mediump float;
-      varying vec2 vUv;
-      uniform sampler2D uTexture;
-      void main() {
-        vec2 center = vec2(0.5, 0.5);
-        vec2 offset = vUv - center;
-        float distance = length(offset);
-        float angle = distance * 10.0;
-        vec2 rotatedUv = vec2(
-          cos(angle) * offset.x - sin(angle) * offset.y,
-          sin(angle) * offset.x + cos(angle) * offset.y
-        ) + center;
-        vec4 color = texture2D(uTexture, rotatedUv);
-        gl_FragColor = color;
-      }
-    `
-  };
+    ${adjustImage}
 
-  const mirror = {
-    uniforms: {
-      uTexture: { value: new TextureLoader().load(imagePath) }
-    },
-    vertexShader: `
-      varying vec2 vUv;
-      void main() {
-        vUv = uv;
-        gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
-      }
-    `,
-    fragmentShader: `
-      precision mediump float;
-      varying vec2 vUv;
-      uniform sampler2D uTexture;
-      void main() {
-        float mirroredX = vUv.x > 0.5 ? 1.0 - vUv.x : vUv.x;
-        vec2 mirroredUv = vec2(mirroredX, vUv.y);
-        vec4 color = texture2D(uTexture, mirroredUv);
-        gl_FragColor = color;
-      }
-    `
-  };
+    void main() {
+      vec4 color = texture2D(uTexture, vUv);
+      vec3 adjustedColor = adjustBrightness(color.rgb, uBrightness);
+      adjustedColor = adjustSaturation(adjustedColor, uSaturation);
+      float gray = dot(adjustedColor, vec3(0.299, 0.587, 0.114));
+      gl_FragColor = vec4(vec3(gray), color.a);
+    }
+  `
+};
+
+const swirl = {
+  ...baseShader,
+  fragmentShader: `
+    precision mediump float;
+    varying vec2 vUv;
+    uniform sampler2D uTexture;
+    uniform float uBrightness;
+    uniform float uSaturation;
+
+    ${adjustImage}
+
+    void main() {
+      vec2 center = vec2(0.5, 0.5);
+      vec2 offset = vUv - center;
+      float distance = length(offset);
+      float angle = distance * 2.0;
+      vec2 rotatedUv = vec2(
+        cos(angle) * offset.x - sin(angle) * offset.y,
+        sin(angle) * offset.x + cos(angle) * offset.y
+      ) + center;
+      vec4 color = texture2D(uTexture, rotatedUv);
+      vec3 adjustedColor = adjustBrightness(color.rgb, uBrightness);
+      adjustedColor = adjustSaturation(adjustedColor, uSaturation);
+      gl_FragColor = vec4(adjustedColor, color.a);
+    }
+  `
+};
+
+const mirror = {
+  ...baseShader,
+  fragmentShader: `
+    precision mediump float;
+    varying vec2 vUv;
+    uniform sampler2D uTexture;
+    uniform float uBrightness;
+    uniform float uSaturation;
+
+    ${adjustImage}
+
+    void main() {
+      float mirroredX = vUv.x > 0.5 ? 1.0 - vUv.x : vUv.x;
+      vec2 mirroredUv = vec2(mirroredX, vUv.y);
+      vec4 color = texture2D(uTexture, mirroredUv);
+      vec3 adjustedColor = adjustBrightness(color.rgb, uBrightness);
+      adjustedColor = adjustSaturation(adjustedColor, uSaturation);
+      gl_FragColor = vec4(adjustedColor, color.a);
+    }
+  `
+};
 
   var shader = grayscale;
   switch (version) {
